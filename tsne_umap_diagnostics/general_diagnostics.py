175ce18303aed323e6f0c4baa6a2aefb3a49fa62
D. Haik <d.haik@umail.leidenuniv.nl>
2025-05-01 15:22:40 +0200

    matrix fit plot

--- tsne_umap_diagnostics/general_diagnostics.py
+++ tsne_umap_diagnostics/general_diagnostics.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sklearn.metrics as metrics
-from .plotting import plot_distances
+from .plotting import plot_distances, plot_similarities
+from .umap_diagnostics import calculate_V_matrix, calculate_W_matrix
+from .tsne_diagnostics import calculate_P_matrix, calculate_Q_matrix
 
 def distance_fit_plot(distances_original=None, distances_embedded=None, X_original=None, X_embedded=None, title='Fit plot of the original and embedded distances'):
     if X_original is not None and X_embedded is not None:
@@ -8,3 +10,25 @@ def distance_fit_plot(distances_original=None, distances_embedded=None, X_origin
         distances_embedded = metrics.pairwise_distances(X_embedded)
 
     return plot_distances(distances_original, distances_embedded, title=title)
+
+def matrix_fit_plot_from_matrices(hd_matrix, ld_matrix, asymmetric_matrix=False, title='Fit plot of similarities in high and low dimensions'):
+    return plot_similarities(hd_matrix=hd_matrix, ld_matrix=ld_matrix, asymmetric_matrix=asymmetric_matrix, title=title)
+
+def matrix_fit_plot(distances_original=None, distances_embedded=None, X_original=None, X_embedded=None,
+                    method='tsne', asymmetric_matrix=False, umap_knn_indices=None, umap_approx_W=False,
+                    perplexity=30, n_steps=100, tolerance = 1e-5, k_neighbours=15, min_dist=0.1, spread=1.0,
+                    title='Fit plot of the similarities in original and embedded space'):
+    if X_original is not None and X_embedded is not None:
+        distances_original = metrics.pairwise_distances(X_original)
+        distances_embedded = metrics.pairwise_distances(X_embedded)
+
+    if method == 'tsne':
+        hd_matrix = calculate_P_matrix(distances_original=distances_original, X_original=X_original, perplexity=perplexity, n_steps=n_steps, tolerance=tolerance)
+        ld_matrix = calculate_Q_matrix(distances_embedded=distances_embedded, X_embedded=X_embedded)
+    elif method == 'umap':
+        hd_matrix = calculate_V_matrix(distances_original=distances_original, indices=umap_knn_indices, X_original=X_original, k_neighbours=k_neighbours, n_steps=n_steps, tolerance=tolerance)
+        ld_matrix = calculate_W_matrix(distances_embedded=distances_embedded, X_embedded=X_embedded, use_approximation=umap_approx_W, min_dist=min_dist, spread=spread)
+    else:
+        raise ValueError('Method must be either "tsne" or "umap"')
+
+    return plot_similarities(hd_matrix=hd_matrix, ld_matrix=ld_matrix, asymmetric_matrix=asymmetric_matrix, title=title)
--- tsne_umap_diagnostics/plotting.py
+++ tsne_umap_diagnostics/plotting.py
@@ -4,16 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_distances(X_original, X_embedded, title):
-
-    # Get the upper triangle from a matrix
-    def upper_tri(A):
-        m = A.shape[0]
-        r = np.arange(m)
-        mask = r[:, None] < r
-        return A[mask]
-
-    X_original = upper_tri(X_original)
-    X_embedded = upper_tri(X_embedded)
+    X_original = _upper_tri(X_original)
+    X_embedded = _upper_tri(X_embedded)
 
     data = pd.DataFrame({
         'Original Distance': X_original,
@@ -28,6 +20,25 @@ def plot_distances(X_original, X_embedded, title):
     ax.set_ylabel("Low-dimensional Distance")
     return fig
 
+def plot_similarities(hd_matrix, ld_matrix, asymmetric_matrix, title):
+    if not asymmetric_matrix:
+        hd_matrix = _upper_tri(hd_matrix)
+        ld_matrix = _upper_tri(ld_matrix)
+
+    data = pd.DataFrame({
+        'Original Similarity': hd_matrix.flatten(),
+        'Similarity in Embedding': ld_matrix.flatten()
+    })
+
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=data, x='Original Similarity', y='Similarity in Embedding', alpha=0.2, ax=ax)
+    sns.despine()
+    ax.set_title(title)
+    ax.set_xlabel("High-dimensional Similarity")
+    ax.set_ylabel("Low-dimensional Similarity")
+    return fig
+
+
 def matrix_heatmap(matrix, title='Matrix heatmap'):
     fig, ax = plt.subplots()
     sns.heatmap(matrix, cmap='YlGnBu', annot=False, fmt='.2f', cbar_kws={'label': 'Similarity'},
@@ -36,3 +47,9 @@ def matrix_heatmap(matrix, title='Matrix heatmap'):
     ax.set_xlabel('Sample')
     ax.set_ylabel('Sample')
     return fig
+
+def _upper_tri(A):
+    m = A.shape[0]
+    r = np.arange(m)
+    mask = r[:, None] < r
+    return A[mask]
